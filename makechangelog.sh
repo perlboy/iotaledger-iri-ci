2f5f48e4566af60973beb672f04aef4cff65b5e6
Stuart Low <stuart.low@me.com>
2017-09-14 15:05:27 +1000

    fix package name

--- makechangelog.sh
+++ makechangelog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo > ../iotaledger-iri-ci/debian/changelog
 prevtag=v1.2.0 
-git tag -l v* | sort -V | while read tag; do     (echo "$pkgname (${tag#v}) unstable; urgency=low"; git log --pretty=format:'  * %s' $prevtag..$tag; git log --pretty='format:%n%n -- %aN <%aE>  %aD%n%n' $tag^..$tag) | cat - ../iotaledger-iri-ci/debian/changelog | sponge ../iotaledger-iri-ci/debian/changelog;         prevtag=$tag; done
+git tag -l v* | sort -V | while read tag; do     (echo "iotaledger-iri (${tag#v}) unstable; urgency=low"; git log --pretty=format:'  * %s' $prevtag..$tag; git log --pretty='format:%n%n -- %aN <%aE>  %aD%n%n' $tag^..$tag) | cat - ../iotaledger-iri-ci/debian/changelog | sponge ../iotaledger-iri-ci/debian/changelog;         prevtag=$tag; done
