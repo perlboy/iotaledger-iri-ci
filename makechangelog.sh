0f7f81ee5ab68298dac039a4167e9b92dc8cfb64
Stuart Low <stuart.low@me.com>
2017-09-14 16:13:24 +1000

    replace unstable keyword with xenial;

--- makechangelog.sh
+++ makechangelog.sh
@@ -2,3 +2,6 @@
 echo > ../iotaledger-iri-ci/debian/changelog
 prevtag=v1.2.0 
 git tag -l v* | sort -V | while read tag; do     (echo "iotaledger-iri (${tag#v}) unstable; urgency=low"; git log --pretty=format:'  * %s' $prevtag..$tag; git log --pretty='format:%n%n -- %aN <%aE>  %aD%n%n' $tag^..$tag) | cat - ../iotaledger-iri-ci/debian/changelog | sponge ../iotaledger-iri-ci/debian/changelog;         prevtag=$tag; done
+
+sed -i -e 's/unstable/xenial/g' ../iotaledger-iri-ci/debian/changelog
+
